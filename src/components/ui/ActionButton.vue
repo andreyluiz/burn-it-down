dbc46a3a56d8537051bb58fe84609dd3de573928
Andrey Luiz <andreyluiz.developer@gmail.com>
2017-06-14 00:14:38 -0300

    Create an action button component.

---
+++ src/components/ui/ActionButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button
+    type="submit"
+    :class="{ 'action-button': true, 'block': block, 'medium': medium }"
+    v-on:click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'action-button',
+  props: {
+    block: {
+      type: Boolean,
+      value: false,
+    },
+    medium: {
+      type: Boolean,
+      value: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.action-button {
+  font: inherit;
+  background-color: #FF5722;
+  border: 1px solid #FF5722;
+  color: white;
+  cursor: pointer;
+  border-radius: 4px;
+  width: 100px;
+  outline: none;
+
+  &.block {
+    width: 100%;
+  }
+
+  &.medium {
+    height: 30px;
+  }
+}
+
+.action-button:hover {
+  background-color: #FF6833;
+}
+
+.action-button:active {
+  background-color: #EE4611;
+}
+</style>
