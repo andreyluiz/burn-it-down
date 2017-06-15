2ca855bffbc1d68d5bcb43376974fd56018685f9
Andrey Luiz <andreyluiz.developer@gmail.com>
2017-06-15 02:11:13 -0300

    Create a text input component.

---
+++ src/components/ui/TextInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <input
+    type="text"
+    :id="id"
+    :name="name"
+    :placeholder="placeholder"
+    :autofocus="autofocus"
+    v-model="email"
+    @change="$emit('change')"
+    @input="$emit('input', email)">
+</template>
+
+<script>
+export default {
+  name: 'text-input',
+  watch: {
+    value(newValue) {
+      this.email = newValue;
+    },
+  },
+  data() {
+    return {
+      email: this.value,
+    };
+  },
+  props: {
+    name: {
+      type: String,
+      value: '',
+    },
+    placeholder: {
+      type: String,
+      value: '',
+    },
+    id: {
+      type: String,
+      value: '',
+    },
+    autofocus: {
+      type: Boolean,
+      value: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+input {
+  flex: 1 1 auto;
+  padding: 4px;
+  font: inherit;
+  color: inherit;
+  border: 1px solid #2c3e50;
+  border-radius: 4px;
+}
+</style>
